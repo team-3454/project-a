0a10f8d30288f4ad8ff8b78b8b57c30413d513db
SinJie Wang <tallgeese0316@gmail.com>
2017-09-30 03:13:48 +0800

    Add proxy server
    新增 proxy server 用於轉送 detie request

---
+++ proxy_serv/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const http = require('http');
+const url = require("url");
+
+const detie = require("./detie-api.js");
+
+const hostname = '0.0.0.0';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname !== '/detie') {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.write("404 Not Found\n");
+    res.end();
+    return;
+  }
+
+  //console.log(parsedUrl.query);
+  const params = parsedUrl.query;
+  detie(params).then(response => {
+    //console.log(response);
+    res.writeHead(200, {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    });
+    res.end(response);
+    console.log('finish!!');
+  }, (err) => {
+    res.writeHead(500, {"Content-Type": "text/plain"});
+    res.write("500 Internal Server Error\n");
+    res.end();
+  })
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
