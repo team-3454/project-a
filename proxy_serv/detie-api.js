5e0443158d4591738d507231ddef22a8cd441fe2
rein <redeyes2015@gmail.com>
2017-09-30 05:46:39 +0800

    Make `sendGet` async function for cleaner code

--- proxy_serv/detie-api.js
+++ proxy_serv/detie-api.js
@@ -82,34 +82,30 @@ const sendGetRequest = (baseUrl, api_key, secret, params) => new Promise((resolv
 const api_key = "1fdeae6e7fd44c9e991d21066a828f0c"
 const secret = "4dae4d6a-4874-4d60-8eac-67701520671d"
 
-module.exports = function (params) {
-  return new Promise((resolve, rej) => {
-    sendGetRequest("https://alpha.api.detie.cn/api/v2/online_solutions", api_key, secret, {
+async function sendGet(params) {
+  const res = await sendGetRequest("https://alpha.api.detie.cn/api/v2/online_solutions", api_key, secret, {
       "from": params.from,//"ST_E020P6M4",
       "to": params.to,//"ST_EMYR64OX",
       "date": params.date,//"2017-11-08",
       "time": params.time,//"11:00",
       "adult": params.adult,//1,
       "child": params.child//0
-    })
-      .then(res => JSON.parse(res).async)
-      .then(async (token) => {
-        while (true) {
-          try {
-            const res = await sendGetRequest("https://alpha.api.detie.cn/api/v2/async_results/" + token, api_key, secret, {
-              async_key: token
-            });
-            return res;
-          } catch (e) {
-            if (e instanceof ErrorAgain) {
-              await new Promise(r => setTimeout(r, 1000));
-              continue;
-            }
-            throw e;
-          }
-        }
-      })
-      .then(res => resolve(res))
-      .catch(err => rej(err));
-  });
+    });
+  const token = JSON.parse(res).async;
+  while (true) {
+    try {
+      const res = await sendGetRequest("https://alpha.api.detie.cn/api/v2/async_results/" + token, api_key, secret, {
+        async_key: token
+      });
+      return res;
+    } catch (e) {
+      if (e instanceof ErrorAgain) {
+        await new Promise(r => setTimeout(r, 1000));
+        continue;
+      }
+      throw e;
+    }
+  }
 }
+
+module.exports = sendGet;
