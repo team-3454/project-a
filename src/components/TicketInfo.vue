2b6094b404921a2a3a8c39c3b66ae605711634bf
crchen <crchern@users.noreply.github.com>
2017-09-30 06:49:30 +0800

    Add property input for TrackInfo

--- src/components/TicketInfo.vue
+++ src/components/TicketInfo.vue
@@ -3,28 +3,28 @@
       <div class="container"> 
         <h1>{{ msg }}</h1>
         <div>
-          <h2>Reference number: {{ RefNumber}}</h2>
+          <h2>Reference number: {{ info.RefNumber}}</h2>
         </div>
         <div class="row">
           <div class="col-md-2">
-            <h4>From: {{From}}</h4>
+            <h4>From: {{info.From}}</h4>
           </div>
           <div class="col-md-2">
-            <h4>To: {{To}}</h4>
+            <h4>To: {{info.To}}</h4>
           </div>
         </div>
         <div>
           <div class="row">
-            <div class="col-md-2"><h4>Date: {{Departure}}</h4> </div>
-            <div class="col-md-2"><h4>Time: {{Arrive}}</h4> </div>
+            <div class="col-md-2"><h4>Date: {{info.Departure}}</h4> </div>
+            <div class="col-md-2"><h4>Time: {{info.Arrive}}</h4> </div>
           </div>
         </div>
         <div>
-          <h4>Bought price: {{BoughtPrice}} </h4> 
+          <h4>Bought price: {{info.BoughtPrice}} </h4> 
           <div>
               <div class="row">
                 <div class="col-md-2"><h4>Sell price:</h4></div>
-                <div class="col-md-2"><input type="text" class="form-control"></div>
+                <div class="col-md-2"><input type="text" class="form-control" :placeholder=info.HighestPrice></div>
                 <div class="col-md-2"><input type="button" class="btn btn-primary" value="Submit"></div>
               </div>
           </div>
@@ -37,15 +37,22 @@
 
 export default {
   name: 'TicketInfo',
+  props: {
+    info: {
+      default: {
+        RefNumber: 'xxxxxxxxxx',
+        From: 'Paris',
+        To: 'Berlin',
+        Departure: 18000000000,
+        Arrive: 18000000001,
+        BoughtPrice: 500,
+        HighestPrice: 800
+      }
+    }
+  },
   data () {
     return {
-      msg: 'Ticket Information',
-      RefNumber: 'xxxxxxxxx',
-      From: 'Paris',
-      To: 'Berlin',
-      Departure: 18000000000,
-      Arrive: 18000000001,
-      BoughtPrice: 500
+      msg: 'Ticket Information'
     }
   }
 }
