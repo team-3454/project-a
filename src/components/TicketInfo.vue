d821126262c565ca3f8a83e29f18235921fe4f5f
crchen <crchern@users.noreply.github.com>
2017-09-30 05:32:48 +0800

    修改TicketInfo.vue排版

--- src/components/TicketInfo.vue
+++ src/components/TicketInfo.vue
@@ -1,39 +1,35 @@
 <template>
   <div class="ticket-info">
-    <div class="info-border">
       <div class="container"> 
         <h1>{{ msg }}</h1>
         <div>
           <h2>Reference number: {{ RefNumber}}</h2>
         </div>
         <div class="row">
-          <div class="col-md-4">
-            <h2>From: {{From}}</h2>
+          <div class="col-md-2">
+            <h4>From: {{From}}</h4>
           </div>
-          <div class="col-md-8">
-            <h2>To: {{To}}</h2>
+          <div class="col-md-2">
+            <h4>To: {{To}}</h4>
           </div>
         </div>
         <div>
-          <h2>Out:</h2>
           <div class="row">
-            <div class="col-md-4"><h3>Date: {{Departure}}</h3> </div>
-            <div class="col-md-4"><h3>Time: {{Arrive}}</h3> </div>
+            <div class="col-md-2"><h4>Date: {{Departure}}</h4> </div>
+            <div class="col-md-2"><h4>Time: {{Arrive}}</h4> </div>
           </div>
         </div>
-        <div> 
-          <h2>Bought price: {{ BoughtPrice}} </h2> 
-        </div>
         <div>
-            <div class="row">
-              <div class="col-md-4"></div><h2>Sell price:</h2></div>
-              <div class="col-md-4"><input type="text" class="form-control"></div>
-              <div class="col-md-4"><input type="button" class="btn btn-primary" value="Submit"></div>
-            </div>
+          <h4>Bought price: {{BoughtPrice}} </h4> 
+          <div>
+              <div class="row">
+                <div class="col-md-2"><h4>Sell price:</h4></div>
+                <div class="col-md-2"><input type="text" class="form-control"></div>
+                <div class="col-md-2"><input type="button" class="btn btn-primary" value="Submit"></div>
+              </div>
+          </div>
         </div>
-        <br>
-      </div>    
-    </div>
+      </div>
   </div>
 </template>
 
@@ -57,7 +53,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.info-border {
-  border-style: solid;
-};
+
 </style>
