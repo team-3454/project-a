f9aef281a85f2abf5d1f01d4def249d682959317
rein <redeyes2015@gmail.com>
2017-09-30 09:06:59 +0800

    Major refactor, also add makeConfirm

--- proxy_serv/detie-api.js
+++ proxy_serv/detie-api.js
@@ -37,86 +37,30 @@ const signature_of = (api_key, secret, params) => {
 class ErrorAgain extends Error {
 }
 
-const sendGetRequest = (baseUrl, api_key, secret, params) => new Promise((resolve, rej) =>{
-  const sign = signature_of(api_key, secret, params);
-  //console.log(sign);
-
-  const queryString = Object.entries(params)
-    .map(([k, v]) => `${k}=${v}`)
-    .join('&');
-  const url = `${baseUrl}?${queryString}`;
-
-  //console.log(url);
+const sendRequest = options => new Promise((resolve, reject) => {
   const receivedData = [];
+  const data = options.data;
+  options.data = undefined;
 
-  const req = https.request({
-      ...urlParse(url),
-      headers: sign,
-    },
-    res => {
+  const req = https.request(options, res => {
       //console.log('statusCode:', res.statusCode);
       const statusCode = res.statusCode
       res.on('data', data => {
         receivedData.push(data);
-        //resolve(data.toString('utf-8'));
       });
       res.on('end', _ => {
         const resText = Buffer.concat(receivedData).toString('utf-8');
         if (statusCode == 423) {
-          rej(new ErrorAgain());
-        }
-        else if (statusCode != 200) {
-          const e = new Error(statusCode);
-          e.statusCode = statusCode;
-          e.message = resText;
-          rej(e);
-        }
-        else {
-          resolve(resText);
-        }
-      });
-    }
-  );
-
-  req.on('error', (e) => {
-    console.error(e);
-    rej(e);
-  });
-  req.end();
-});
-
-const sendPostRequest = (baseUrl, api_key, secret, params) => new Promise((resolve, rej) =>{
-  const sign = signature_of(api_key, secret, params);
-  sign["Content-Type"] = "application/json";
-  console.log(sign);
-
-  const receivedData = [];
-
-  const req = https.request({
-      ...urlParse(baseUrl),
-      method: 'POST',
-      headers: sign,
-    },
-    res => {
-      console.log('statusCode:', res.statusCode);
-      const statusCode = res.statusCode
-      res.on('data', data => {
-        receivedData.push(data);
-        //resolve(data.toString('utf-8'));
-      });
-      res.on('end', _ => {
-        const resText = Buffer.concat(receivedData).toString('utf-8');
-        if (statusCode == 423) {
-          rej(new ErrorAgain());
+          reject(new ErrorAgain());
         }
         else if (statusCode < 200 || statusCode > 299) {
           const e = new Error(statusCode);
           e.statusCode = statusCode;
-          e.message = resText;
-          rej(e);
+          e.message = resText.slice(0, 40);
+          e.fullMessage = resText;
+          reject(e);
         }
         else {
-          console.log(res.headers)
           resolve(resText);
         }
       });
@@ -125,25 +69,43 @@ const sendPostRequest = (baseUrl, api_key, secret, params) => new Promise((resol
 
   req.on('error', (e) => {
     console.error(e);
-    rej(e);
+    reject(e);
   });
-  req.write(JSON.stringify(params));
+  if (data) {
+    req.write(data);
+  }
   req.end();
 });
+
+const sendGetRequest = (url, api_key, secret, params) => {
+  const sign = signature_of(api_key, secret, params);
+  //console.log(sign);
+
+  return sendRequest({
+    ...urlParse(url),
+    headers: sign,
+  });
+};
+
+const sendPostRequest = (baseUrl, api_key, secret, params) => {
+  const sign = signature_of(api_key, secret, params);
+  sign["Content-Type"] = "application/json";
+  //console.log(sign);
+
+  const receivedData = [];
+
+  return sendRequest({
+    ...urlParse(baseUrl),
+    method: 'POST',
+    headers: sign,
+    data: JSON.stringify(params)
+  })
+};
 
 const api_key = "1fdeae6e7fd44c9e991d21066a828f0c"
 const secret = "4dae4d6a-4874-4d60-8eac-67701520671d"
 
-async function sendGet(params) {
-  const res = await sendGetRequest("https://alpha.api.detie.cn/api/v2/online_solutions", api_key, secret, {
-      "from": params.from,//"ST_E020P6M4",
-      "to": params.to,//"ST_EMYR64OX",
-      "date": params.date,//"2017-11-08",
-      "time": params.time,//"11:00",
-      "adult": params.adult,//1,
-      "child": params.child//0
-    });
-  const token = JSON.parse(res).async;
+async function pollingResults (token) {
   while (true) {
     try {
       const res = await sendGetRequest("https://alpha.api.detie.cn/api/v2/async_results/" + token, api_key, secret, {
@@ -161,8 +123,29 @@ async function sendGet(params) {
   }
 }
 
-async function sendPost(params) {
-  const res = await sendPostRequest("https://alpha.api.detie.cn/api/v2/online_orders", api_key, secret, {
+async function searchSolutions(params) {
+  /* params:{
+      "from": params.from,//"ST_E020P6M4",
+      "to": params.to,//"ST_EMYR64OX",
+      "date": params.date,//"2017-11-08",
+      "time": params.time,//"11:00",
+      "adult": params.adult,//1,
+      "child": params.child//0
+    }*/
+  const baseUrl = "https://alpha.api.detie.cn/api/v2/online_solutions";
+  const queryString = Object.entries(params)
+    .map(([k, v]) => `${k}=${v}`)
+    .join('&');
+  const url = `${baseUrl}?${queryString}`;
+
+  const res = await sendGetRequest(url, api_key, secret, params);
+
+  return await pollingResults(JSON.parse(res).async);
+}
+
+async function makeBooking(params) {
+  /* params:
+  {
     "contact": {
       "name": "Liping",
       "email": "lp@163.com",
@@ -170,41 +153,43 @@ async function sendPost(params) {
       "address": "beijing",
       "postcode": "100100"
     },
-    "passengers": [
-      {
-        "last_name": "zhang",
-        "first_name": "san",
-        "birthdate": "1986-09-01",
-        "passport": "A123456",
-        "email": "x@a.cn",
-        "phone": "15000367081",
-        "gender": "male"
-      }
-    ],
-    "sections": [
-      "P_1LDC989",
-    ],
-    "seat_reserved": true
-    });
-  const token = JSON.parse(res).async;
-  while (true) {
-    try {
-      const res = await sendGetRequest("https://alpha.api.detie.cn/api/v2/async_results/" + token, api_key, secret, {
-        async_key: token
-      });
-      return res;
-    } catch (e) {
-      if (e instanceof ErrorAgain) {
-        console.log('retrying...')
-        await new Promise(r => setTimeout(r, 1000));
-        continue;
-      }
-      throw e;
+      "passengers": [
+        {
+          "last_name": "zhang",
+          "first_name": "san",
+          "birthdate": "1986-09-01",
+          "passport": "A123456",
+          "email": "x@a.cn",
+          "phone": "15000367081",
+          "gender": "male"
+        }
+      ],
+      "sections": [
+        booking_code,// WARN: this need to be copied from recent `searchSolutions`
+      ],
+      "seat_reserved": true
+  }*/
+  const res = await sendPostRequest("https://alpha.api.detie.cn/api/v2/online_orders", api_key, secret, params);
+  return await pollingResults(JSON.parse(res).async);
+}
+
+async function makeConfirm(confirmID, params) {
+  /* params:
+    {
+      credit_card: {
+              number: "37887690145",
+                exp_month: 11,
+                exp_year: 20,
+                cvv: "123"
+            }
     }
-  }
+  */
+  const res = await sendPostRequest(`https://alpha.api.detie.cn/api/v2/${confirmID}/online_confirmations`, api_key, secret, params);
+  return await pollingResults(JSON.parse(res).async);
 }
 
 module.exports = {
-  sendGet,
-  sendPost,
+  searchSolutions,
+  makeBooking,
+  makeConfirm,
 };
