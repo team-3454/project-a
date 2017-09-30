8dd9f3ff3473ca973cd3dd9a46aca7409374a070
crchen <crchern@users.noreply.github.com>
2017-09-30 16:37:14 +0800

    Support summit function

--- src/components/sell.vue
+++ src/components/sell.vue
@@ -11,8 +11,8 @@
           <div class="col-md-5" v-on:click = "fillPrice(info)"><h4>Ask price: {{info.Ask}}</h4></div>
         </div>
         <div class="row">
-          <div class="col-md-9"><input type="text" class="form-control" v-model="info.CurrentSelectPrice"></div>
-          <div class="col-md-3"><input type="button" class="btn btn-primary" value="Submit"></div>
+          <div class="col-md-9"><input id="input-price" type="text" class="form-control" v-model="info.CurrentSelectPrice"></div>
+          <div class="col-md-3"><input type="button" class="btn btn-primary" v-on:click="onSummitClick(info)" value="Submit"></div>
         </div>
       </div>
     </div>
@@ -21,6 +21,7 @@
 
 <script>
 import TicketInfo from './TicketInfo'
+import $ from 'jquery'
 
 var myInfos = [
   {
@@ -90,6 +91,17 @@ export default {
           alert('已成功以' + data.Ask + '掛單!!')
         }
       }, 0)
+    },
+    onSummitClick: function (ticket) {
+      console.log($('#input-price')[0].value)
+      var userPrice = $('#input-price')[0].value
+      if ($('#input-price')[0].value > ticket.Bid) {
+        if (confirm('是否以價格' + userPrice + '賣出？') === true) {
+          alert('已成功以' + userPrice + '掛單!!')
+        }
+      } else {
+        this.deal(ticket)
+      }
     }
   },
   data () {
